87b59ccc7c8b6195c39c3bb662351065540cc618
Niccolo Madamba <nixx1424@gmail.com>
2014-12-03 08:31:13 -0800

    first commit

---
+++ sites/all/themes/playbox_theme/js/custom.js
@@ -0,0 +1,107 @@
+// We define a function that takes one parameter named $.
+(function ($) {
+	/*============================================
+	Navigation Functions
+	==============================================*/
+	if ($(window).scrollTop()===0){
+		$('#main-nav').removeClass('scrolled');
+	}
+	else{
+		$('#main-nav').addClass('scrolled');
+	}
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop()===0){
+			$('#main-nav').removeClass('scrolled');
+			toggleLogo(true);
+		}
+		else{
+			$('#main-nav').addClass('scrolled');
+			toggleLogo(false);
+		}
+	});
+
+	$('#site-nav').width('100px');
+
+  /**
+   * Toggle the hidden class on both logos so we show one and hide the other.
+   */
+  function toggleLogo(remove){
+    var hidden = 'hidden';
+    $('#logo-dark').toggleClass(hidden, remove);
+		$('#logo-white').toggleClass(hidden, !remove);
+  }
+  
+	/*============================================
+	ScrollTo Links
+	==============================================*/
+	$('a.scrollto').click(function(e){
+		$('html,body').scrollTo(this.hash, this.hash, {gap:{y:-50},animation:  {easing: 'easeInOutCubic', duration: 1600}});
+		e.preventDefault();
+
+		if ($('.navbar-collapse').hasClass('in')){
+			$('.navbar-collapse').removeClass('in').addClass('collapse');
+		}
+	});
+
+	/*============================================
+	Tooltips
+	==============================================*/
+	$("[data-toggle='tooltip']").tooltip();
+
+	/*============================================
+	Placeholder Detection
+	==============================================*/
+
+	if (!Modernizr.input.placeholder) {
+		$('#contact-form').addClass('no-placeholder');
+	}
+
+	/*============================================
+	Scrolling Animations
+	==============================================*/
+
+	$('.scrollimation').waypoint(function(){
+		$(this).addClass('in');
+	},{offset:function(){
+			var h = $(window).height();
+			var elemh = $(this).outerHeight();
+			if ( elemh > h*0.3){
+				return h*0.7;
+			}else{
+				return h - elemh;
+			}
+		}
+	});
+
+	/*============================================
+	Resize Functions
+	==============================================*/
+	$(window).resize(function(){
+		scrollSpyRefresh();
+		waypointsRefresh();
+	});
+	/*============================================
+	Refresh scrollSpy function
+	==============================================*/
+	function scrollSpyRefresh(){
+		setTimeout(function(){
+			$('body').scrollspy('refresh');
+		},1000);
+	}
+
+	/*============================================
+	Refresh waypoints function
+	==============================================*/
+	function waypointsRefresh(){
+		setTimeout(function(){
+			$.waypoints('refresh');
+		},1000);
+	}
+
+	/*============================================
+	Handle logo swapping for large screens
+	==============================================*/
+  
+
+}(jQuery));
